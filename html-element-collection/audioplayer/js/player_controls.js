2ce96af0bfbb8d0e8cd036a16fa296196fe8cd1d
m\masya <x-modem@yandex.ru>
2018-12-05 21:57:48 +0300

    HW2-Release2

--- html-element-collection/audioplayer/js/player_controls.js
+++ html-element-collection/audioplayer/js/player_controls.js
@@ -1,11 +1,17 @@
 'use strict';
 
-const playerStatus = document.getElementsByClassName('mediaplayer');
+const trackSet = [
+  {title: "LA Chill Tour", src: "https://d34x6xks9kc6p2.cloudfront.net/540997b0-a35f-4b69-86d6-b1c925c4a264/540997b0-a35f-4b69-86d6-b1c925c4a264.mp3"},
+  {title: "This is it band", src: "https://netology-code.github.io/hj-homeworks/html-element-collection/audioplayer/mp3/This is it band.mp3"},
+  {title: "LA Fusion Jam", src: "https://netology-code.github.io/hj-homeworks/html-element-collection/audioplayer/mp3/LA Fusion Jam.mp3"}
+];
 
+const playerStatus = document.getElementsByClassName('mediaplayer');
 const tracks = document.getElementsByTagName('audio');
+
 var player = tracks[0];
 var currentTrackIndex = 0;
-var len = tracks.length;
+var len = trackSet.length;
 var trackTitle = document.getElementsByClassName('title')[0];
 
 const playStateButton = document.getElementsByClassName('playstate');
@@ -16,31 +22,31 @@ const backButton = document.getElementsByClassName('back');
 const nextButton = document.getElementsByClassName('next');
 
 function titleSet() {
+  tracks[0].title = trackSet[currentTrackIndex].title;
+  tracks[0].src = trackSet[currentTrackIndex].src;
   trackTitle.title = player.title;
 }
 
 function back() {
-  stop();
   if (currentTrackIndex === 0) {
     currentTrackIndex = len - 1;
-    player = tracks[currentTrackIndex];
-    play();
   } else {
     currentTrackIndex = currentTrackIndex - 1;
-    player = tracks[currentTrackIndex];
+  }
+  titleSet();
+  if (playerStatus[0].classList.contains('play')) {
     play();
   }
 }
 
 function next() {
-  stop();
   if (currentTrackIndex === len - 1) {
     currentTrackIndex = 0;
-    player = tracks[currentTrackIndex];
-    play();
   } else {
     currentTrackIndex = currentTrackIndex + 1;
-    player = tracks[currentTrackIndex];
+  }
+  titleSet();
+  if (playerStatus[0].classList.contains('play')) {
     play();
   }
 }
--- html-element-collection/drum-machine/js/drum-kit__drum.js
+++ html-element-collection/drum-machine/js/drum-kit__drum.js
@@ -4,6 +4,7 @@ const mainButtonList = document.getElementsByClassName('drum-kit__drum');
 
 function soundPlay() {
   const player = this.getElementsByTagName('audio');
+  player[0].currentTime = 0;
   player[0].play();
 }
 
