65a5fdacc345712a665c947743a3c9d222231cdc
m\masya <x-modem@yandex.ru>
2018-12-15 17:50:00 +0300

    HW8-Release1

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,14 @@
 'use strict';
+
+function handleItemsClick(event) {
+  if (!(event.target.classList.contains('add-to-cart'))) {
+    return;
+  }
+  let item = {'title': event.target.dataset.title,
+              'price': event.target.dataset.price
+  };
+  addToCart(item);
+}
+
+const main = document.querySelector('main');
+main.addEventListener('click', handleItemsClick);
--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,19 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+  if (event.target.tagName !== 'TH') {
+    return;
+  }
+  if (!(event.target.hasAttribute('data-dir'))) {
+    event.target.setAttribute('data-dir', 1);
+  } else if (event.target.dataset.dir === '1') {
+    event.target.dataset.dir = -1;
+  } else {
+    event.target.dataset.dir = 1;
+  }
+  tableElement.setAttribute('data-sort-by', event.target.dataset.propName);
+  sortTable(event.target.dataset.propName, event.target.dataset.dir);
 }
+
+const tableElement = document.querySelector('table');
+tableElement.addEventListener('click', handleTableClick);
